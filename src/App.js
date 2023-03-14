8e8c5bff9f0bdb027a2d631c37298c2081bdbd7c
SaidGM <said.mg.1517@gmail.com>
2023-03-14 14:19:11 +0100

    correction

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <Route path="/" element={<MainPage />} />
       <Route path="/infoShoe/:id" element={<InfoPage />} />
       <Route path="/admin" element={<AdminPage />} />
-    </Routes>
+    </Routes>..
   );
 }
 
