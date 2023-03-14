dca3bed4e76388efc0a753e9b8f3dce3bcd47ee8
SaidGM <said.mg.1517@gmail.com>
2023-03-14 16:05:32 +0100

    PrincipalUpdate

--- src/components/bannerFeelYourShoes/BannerFeelYourShoes.jsx
+++ src/components/bannerFeelYourShoes/BannerFeelYourShoes.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
+import './BannerFeelYourShoes.css'
+import topImg from '../../assets/webImg/feelYourShoes.jpg'
+import bannerImg from '../../assets/webImg/freeShipping.jpg'
 
 function BannerFeelYourShoes() {
   return (
-    <div>
-      
+    <>
+    <div className='bannerFeel'>
+      <img src={topImg} alt="" className='topImg'/>
+      <h1 className='titleGalactica'>GALACTICA</h1>
+      <div className='feelYourSection'>
+        <h3 className='caption'>A NEW WAY TO FEEL YOUR <br/> SHOES</h3>
+        <button className='shopNowButton'>SHOP NOW</button>
+        <button className='ourNowButton'>OUR TEAM</button>
+      </div>
     </div>
+    <div className='trendSection'>
+      <article className='articleSnickers'>
+        <img src="https://www.tint-store.com/de-de/Data/Images/Big/adidas-originals-forum-low-weiss-schwarz-fy7757-lifestyle_right_back.jpg" alt="" className='articleImg'/>
+        <div className='containerInfo'>
+          <div className='infoAndPrice'>
+            <p className='titleArticle'>FORUM LOW</p>
+            <p className='priceArticle'>$110</p>
+          </div>
+          <button className='buttonShop'>SHOP</button>
+        </div>
+      </article>
+      <article className='articleSnickers'>
+        <img src="https://assets.adidas.com/images/w_600,f_auto,q_auto/87e278ebf8cf4706a3bda7fa00e450a5_9366/Busenitz_Pro_Schuh_Schwarz_G48060_01_standard.jpg" alt="" className='articleImg'/>
+        <div className='containerInfo'>
+          <div className='infoAndPrice'>
+            <p className='titleArticle'>BUSENITZ</p>
+            <p>$130</p>
+          </div>
+          <button className='buttonShop'>SHOP</button>
+        </div>
+      </article>
+    </div>
+    <div className='banner'>
+      <img src={bannerImg} alt="" className='bannerImg'/>
+      <h1 className='upBanner'>WORLDWIDE FREE <br/> SHIPPING TAX</h1>
+    </div>
+    </>
   )
 }
 
