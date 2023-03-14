0f4cb9f9ed192221adc9e7827b2009eeb4fa7679
javier <villarestrepocarlos@gmail.com>
2023-03-14 15:03:05 +0100

    shopPages

---
+++ src/pages/shopPage/ShopPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function ShopPage() {
+  return (
+    
+    <div className='shopPage'>
+  
+      <div className='productCard'>
+        <div className='imgBox'>
+          <img className='productImg'  src="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/b7beee7c32d4438aaba3acb6001c2e7b_9366/Zapatilla_Forum_Low_Blanco_FY7757_01_standard.jpg" alt="Product" />
+        </div>
+        <div className='productInfo'>
+          <h3 className='model'>Model</h3>
+          <h5 className='brand'>Brand</h5>
+          <p className='price'>Price</p>
+        </div>
+      </div>
+    </div>
+  )
+}
