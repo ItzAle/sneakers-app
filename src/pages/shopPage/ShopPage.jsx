55578ed15e89bfd811c8e2f950bbf308218d98fa
SaidGM <said.mg.1517@gmail.com>
2023-03-22 11:23:51 +0100

    iconsColor

--- src/pages/shopPage/ShopPage.jsx
+++ src/pages/shopPage/ShopPage.jsx
@@ -31,8 +31,8 @@ export default function ShopPage() {
       {sneakers.map((item) =>{
         return(
           <div className='productCard'>
-            <Icon icon="mdi:cards-heart" color="gray" width="28" height="28" className='iconHeart' />
-            <Icon icon="material-symbols:shopping-cart-checkout" color="gray" width="30" height="30" className='iconShop' />
+            <Icon icon="mdi:cards-heart" width="29" height="29" className='iconHeart' />
+            <Icon icon="material-symbols:shopping-cart-checkout"  width="31" height="31" className='iconShop' />
             <Link to={`/infoShoe/${item.id}`}>
               <div className='imgBox'>
                 <img className='productImg' src={item.img} alt="Product" />
