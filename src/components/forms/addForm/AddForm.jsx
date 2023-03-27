48a35d36ecac4560c24675887da736e09786ab53
SaidGM <said.mg.1517@gmail.com>
2023-03-27 15:24:35 +0200

    version1

--- src/components/forms/addForm/AddForm.jsx
+++ src/components/forms/addForm/AddForm.jsx
@@ -36,7 +36,7 @@ function AddForm() {
         const form = document.querySelector('.addForm')
         form.style.display = 'none';
     }
-
+adwainidfa
     const handleSneakerChange = (e) =>{
         let name = e.target.name;
         setSneaker({ 
