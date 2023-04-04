6871cced2c83e0703e98485e6dd394c9a95e15bc
SaidGM <said.mg.1517@gmail.com>
2023-04-04 10:03:30 +0200

    validates

--- src/pages/loginPage/Login.jsx
+++ src/pages/loginPage/Login.jsx
@@ -3,29 +3,40 @@ import './Login.css'
 import { Link } from 'react-router-dom'
 
 function Login() {
+    function validateLoginForm() {
+        var x = document.forms["Form"]["Email"].value;
+        var y = document.forms["Form"]["password"].value;
+        if (x === "") {
+            alert("Name must be filled out.");
+            return false;
+        }
+        else if (y.length < 8)
+            {
+            alert("Password must be at least 8 characters long.");
+            return false;
+        }
+        }
   return (
       <div id='login'>
-          <div id='formLogin'>
+          <form id='formLogin' name="Form" onSubmit={validateLoginForm}>
 
               <h1 className='titleWelcome'>Welcome to Galactica</h1>
 
               <div className='inputContainer'>
-                  <input type="text" className='inputLogin' placeholder='' />
-                  <label htmlFor="labelLogin" className='labelLogin'>Email</label>
+                  <input type="text" className='inputLogin' name="Email"/>
+                  <label htmlFor="labelLogin" className='labelLogin' >Email</label>
               </div>
 
               <div className='inputContainer'>
-                  <input type="password" className='inputLogin' placeholder='' />
-                  <label htmlFor="labelLogin" className='labelLogin'>Password</label>
+                  <input type="password" className='inputLogin' name="password"/>
+                  <label htmlFor="labelLogin" className='labelLogin' >Password</label>
               </div>
               <div className='containerButons'>
-                  <Link to="/">
-                      <input type="submit" className='submitButton' value="Sign In" />
-                  </Link>
+                    <input type="submit" className='submitButton' value="Sign In" />
                   <h5 className='registerNow'>Not a member yet? <Link to={"/register"}><span>Register now</span></Link> </h5>
               </div>
 
-          </div>
+          </form>
       </div>
   )
 }
--- src/pages/registerPage/Register.jsx
+++ src/pages/registerPage/Register.jsx
@@ -4,36 +4,59 @@ import './Register.css'
 import { Link } from 'react-router-dom'
 
 function Register() {
+    function validateRegisterForm() {
+        var x = document.forms["Form"]["name"].value;
+        var y = document.forms["Form"]["email"].value;
+        var z = document.forms["Form"]["password"].value;
+        if (x === "" || y === "") {
+            alert("Name and Email must be filled out.");
+            return false;
+        }
+        else if (z.length < 8)
+            {
+            alert("Password must be at least 8 characters long.");
+            return false;
+        }
+        }
+        function validateEmail(){
+            var emailField = document.getElementById('user-email');
+            var validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
+            if( validEmail.test(emailField.value) ){
+                return true;
+            }else{
+                alert('Email is invalid');
+                return false;
+            }
+        }
   return (
       <div id='login'>
-          <div id='formLogin'>
+          <form id='formLogin' name='Form' onSubmit={validateRegisterForm}>
 
               <h1 className='titleWelcome'>Welcome to Galactica</h1>
 
               <div className='inputContainer'>
-                  <input type="text" className='inputLogin' placeholder='' />
+                  <input type="text" className='inputLogin' name='name' id='user-email'/>
                   <label htmlFor="labelLogin" className='labelLogin'>Name</label>
               </div>
 
               <div className='inputContainer'>
-                  <input type="text" className='inputLogin' placeholder='' />
+                  <input type="text" className='inputLogin' name='email' />
                   <label htmlFor="labelLogin" className='labelLogin'>Email</label>
               </div>
 
               <div className='inputContainer'>
-                  <input type="password" className='inputLogin' placeholder='' />
+                  <input type="password" className='inputLogin' name='password' />
                   <label htmlFor="labelLogin" className='labelLogin'>Password</label>
               </div>
 
               
               <div className='containerButons'>
-                  <Link to="/">
-                      <input type="submit" className='submitButton' value="Register" />
-                  </Link>
-                  <h5 className='loginNow'>Are you already a member? <Link to={"/login"}><span>Login now</span></Link> </h5>
+                    <input type="submit" className='submitButton' value="Register" onClick={validateEmail}/>
+                    <h5 className='loginNow'>Are you already a member? <Link to={"/login"}><span>Login now</span></Link> </h5>
               </div>
 
-          </div>
+          </form>
       </div>
   )
 }
