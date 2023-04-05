d60b4ec82e23b23fb1a1587c75336652178cde37
SaidGM <said.mg.1517@gmail.com>
2023-04-05 17:15:18 +0200

    Login

--- src/pages/loginPage/Login.jsx
+++ src/pages/loginPage/Login.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
-import { Link} from "react-router-dom";
+import { Link, useNavigate} from "react-router-dom";
 import validateEmail from "../../components/validation/EmailValidation";
 import validatePass from "../../components/validation/PasswordValidation";
 import validateLoginForm from "../../components/validation/LoginFormValidation";
+import useLocalStorage  from "../../hooks/useLocalStorage";
 
 function Login() {
+
+  const userModel = {
+    email : "",
+    password : "",
+}
+
+  const [user, setUser] = useState(userModel);
+  let navigate = useNavigate();
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    localStorage.setItem(`${user.email}`)
+    navigate("/shop")
+  }
+
+  const handleOnChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value});
+  }
+
   return (
     <div id="login">
       <form id="formLogin" name="Form" onSubmit={validateLoginForm}>
@@ -13,7 +33,14 @@ function Login() {
         <h1 className="titleWelcome">Welcome to Galactica</h1>
 
         <div className="inputContainer">
-          <input type="text" className="inputLogin" name="Email" id="user-email" onKeyUp={validateEmail} />
+          <input 
+          type="text" 
+          className="inputLogin" 
+          name="Email" 
+          id="user-email" 
+          onKeyUp={validateEmail} 
+          onChange={handleOnChange}
+          />
           <label htmlFor="labelLogin" className="labelLogin"> Email </label>
           <span id="error-msg"></span>
         </div>
@@ -30,7 +57,7 @@ function Login() {
               type="submit"
               className="submitButton"
               value="Sign In"
-              onClick={""}
+              onClick={handleSubmit}
             />
           </Link>
           
